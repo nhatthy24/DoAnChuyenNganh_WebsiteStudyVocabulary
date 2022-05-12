c6df924216e8bb7828794d54aaa3592815b25747
dukhanh <18130108@st.hcmuaf.edu.vn>
2022-05-12 16:51:25 +0700

    add auto flip card

--- template/assets/js/remember_card.js
+++ template/assets/js/remember_card.js
@@ -6,9 +6,16 @@ const editWordDialog = document.getElementById("edit__word-dialog");
 let inputWord = document.getElementById('edit__word-dialog-item-input-word');
 let inputDefinition = document.getElementById('edit__word-dialog-item-input-definition');
 
+var actionFlip = null;
+var actionExchange = null;
+
+
 const cards = document.querySelectorAll('.card__item');
 const btnPreCard = document.querySelector('.card__toolbar-nav-left');
 const btnNextCard = document.querySelector('.card__toolbar-nav-right');
+const btnStart = document.getElementById('control__main-action-start');
+const toolbar = document.getElementById('card__toolbar');
+const repeatCardBtn = document.getElementById('card__item-repeat-btn');
 
 let currentCard = 1;
 const totalIndex = cards.length;
@@ -21,8 +28,8 @@ function preCard() {
         if (currentCard < 1) {
             currentCard = 1;
         }
-        switchCard(currentCard);
-        cards[currentCard -1 ].classList.add('term__card-item--slide-prev')
+        switchCard(currentCard, 'previous');
+        // cards[currentCard - 1].classList.add('term__card-item--slide-prev')
     }
 }
 
@@ -32,8 +39,8 @@ function nextCard() {
         if (currentCard > totalIndex) {
             currentCard = totalIndex;
         }
-        switchCard(currentCard);
-        cards[currentCard -1 ].classList.add('term__card-item--slide-next')
+        switchCard(currentCard, 'next');
+        // cards[currentCard - 1].classList.add('term__card-item--slide-next')
     }
 
 }
@@ -44,19 +51,23 @@ function nextCard() {
 function exchangeWordToDefinition() {
     for (let i = 0; i < cards.length; i++) {
         if (cards[i].style.display == 'block') {
-            const flipBtn = cards[i].querySelector('.card__item-word');
-            const btnEdit = flipBtn.querySelector('.card__item-control-edit-btn');
-            const btnSpeech = flipBtn.querySelector('.card__item-control-speech-btn');
-            const word = cards[i].querySelector('.card__item-side-word').innerHTML.trim();
-            const definition = cards[i].querySelector('.card__item-side-definition').innerHTML.trim();
-            flipBtn.onclick = (e) => {
-                if (e.target === btnEdit) {
-                    openEditWordDialog(word, definition);
-                } else if (e.target === btnSpeech) {
-                    speech(word);
-                } else {
-                    addRotateCard(flipBtn.parentElement);
+            if (!cards[i].classList.contains('card__item-special')) {
+                const flipBtn = cards[i].querySelector('.card__item-word');
+                const btnEdit = flipBtn.querySelector('.card__item-control-edit-btn');
+                const btnSpeech = flipBtn.querySelector('.card__item-control-speech-btn');
+                const word = cards[i].querySelector('.card__item-side-word').innerHTML.trim();
+                const definition = cards[i].querySelector('.card__item-side-definition').innerHTML.trim();
+                flipBtn.onclick = (e) => {
+                    if (e.target === btnEdit) {
+                        openEditWordDialog(word, definition);
+                    } else if (e.target === btnSpeech) {
+                        speech(word);
+                    } else {
+                        addRotateCard(flipBtn.parentElement);
+                    }
                 }
+            } else {
+
             }
         }
     }
@@ -130,12 +141,22 @@ function exchangeCard() {
 
 
 
-function switchCard(currentCard) {
+function switchCard(currentCard, direction) {
     for (let i = 0; i < cards.length; i++) {
         cards[i].style.display = 'none';
         removeRotateCard(cards[i]);
     }
     cards[currentCard - 1].style.display = 'block';
+    if (currentCard === cards.length) {
+        toolbar.style.display = 'none';
+    }
+    if (direction != null) {
+        if (direction === 'previous') {
+            cards[currentCard - 1].classList.add('term__card-item--slide-prev');
+        } else {
+            cards[currentCard - 1].classList.add('term__card-item--slide-next');
+        }
+    }
     exchangeWordToDefinition();
     exchangeDefinitionToWord();
     checkButtonAbility();
@@ -150,12 +171,12 @@ function progressControl() {
     let progressTotal = document.getElementById("progress__total");
     const progressBar = document.getElementById("control__main-progress-bar-fill");
     progressCurrent.innerHTML = currentCard;
-    progressTotal.innerHTML = cards.length;
-    progressBar.style.width = (currentCard / cards.length) * 100 + '%';
+    progressTotal.innerHTML = cards.length - 1;
+    progressBar.style.width = (currentCard / (cards.length - 1)) * 100 + '%';
 }
 
 function openEditWordDialog(word, definition) {
-   
+
     modalInner.appendChild(editWordDialog);
     modal.style.display = 'block';
     editWordDialog.style.display = 'block'
@@ -170,6 +191,66 @@ function closeEditWordDialog() {
 }
 
 
+function startLearning() {
+    if (currentCard === cards.length) {
+        currentCard = 0;
+        startLearning();
+    } else {
+        actionFlip = setTimeout(function () {
+            addRotateCard(cards[currentCard - 1]);
+        }, 2000);
+        actionExchange = setInterval(function () {
+            if (currentCard < cards.length) {
+                setTimeout(() => {
+                    if (currentCard !== cards.length) {
+                        addRotateCard(cards[currentCard - 1]);
+                    }
+                }, 2000);
+                currentCard += 1;
+                switchCard(currentCard, 'next');
+            }
+        }, 4000);
+    }
+
+}
+
+function stopLearning() {
+    clearTimeout(actionFlip);
+    clearInterval(actionExchange);
+    actionFlip = null;
+    actionExchange = null;
+}
+
+
+function startEvent() {
+    btnStart.onclick = () => {
+        if (!btnStart.classList.contains('control__main-action--active')) {
+            btnStart.classList.add('control__main-action--active');
+            startLearning();
+        } else {
+            btnStart.classList.remove('control__main-action--active');
+            stopLearning();
+        }
+
+    }
+}
+
+
+function repeatCard() {
+    repeatCardBtn.onclick = () => {
+        if (!btnStart.classList.contains('control__main-action--active')) {
+            currentCard = 1;
+            toolbar.style.display = 'block';
+            stopLearning();
+            switchCard(currentCard);
+        } else {
+            currentCard = 0;
+            stopLearning();
+            startLearning();
+        }
+    }
+}
+
 switchCard(currentCard);
 exchangeCard();
 exchangeWordToDefinition();
@@ -178,3 +259,5 @@ checkButtonAbility();
 preCard();
 nextCard();
 progressControl();
+startEvent();
+repeatCard();
